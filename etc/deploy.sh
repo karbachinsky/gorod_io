ffab3e447e46721dce09a60fe9b45b9ec683cbdf
Igor Karbachinsky <igorkarbachinsky@mail.ru>
2014-03-24 00:54:20 +0400

    deploy script

---
+++ etc/deploy.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# Deployment script for gorod.io 
+#
+# Author: I. Karbachinsky
+
+set -e
+set -o pipefail
+set -x
+
+
+# production server
+host="81.222.134.119"
+# ssh port
+port=2222
+# ssh user
+ssh_user="gorodin"
+# app directory path
+app_dir="/home/g/gorodin/gorod.in"
+
+ssh_opts="-p $port"
+ssh_key=""
+
+read -s -p "ssh password: " password
+
+
+if [ -z "$password" ]; then
+    echo "Plese specify ssh password! ./deploy.sh *******" >&2 
+    exit 1
+fi
+
+
+execute_remote() {
+    host=$1
+    command=$2
+    error_message=$3
+    # Message used in local mode
+    local_error_message=$4
+    return_error_code=$5
+
+    if [[ -z "$host" || -z "$command" ]]; then
+        return 1
+    fi
+
+    server=`echo "$host" | sed "s/.*@//g"`
+
+    if [ "$server" != "localhost" ]; then
+        sshpass -p $password ssh $ssh_opts $ssh_key "$ssh_user"@"$host" "$command"
+    else
+        eval $command
+    fi
+
+    local RETURN_VALUE="$?"
+    if [ "$RETURN_VALUE" != 0 ] ; then
+        if [[ "$server_name" != "localhost" || -z "$local_error_message" ]]; then
+            if [ ! -z "$error_message" ]; then
+                echo "$error_message" >&2
+            fi
+        else
+            echo "$local_error_message" >&2
+        fi
+
+        if [ "$return_error_code" != "1" ]; then
+            exit $RETURN_VALUE
+        else
+            return $RETURN_VALUE
+        fi
+    fi
+
+    return 0    
+}
+
+
+# Updating files
+execute_remote $host  "cd $app_dir; git reset --hard; git pull origin"
+
+# Updating db
+execute_remote $host "cd $app_dir; ./manage.py schemamigration gorod --auto"
+execute_remote $host "cd $app_dir; /manage.py migrate gorod"
+
+# Updating static
+execute_remote $host "cd $app_dir; ./manage.py collectstatic"
+
+# Updating settings.py
+execute_remote $host "cd $app_dir; cp gorod_io/settings.py.production gorod_io/settings.py"
+
+# Restart wsgi
+execute_remote $host "cd $app_dir; touch index.wsgi"
+

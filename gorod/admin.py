c6ae8ccd01aa463c306cac3d54cecfe39dbe5c81
Igor Karbachinsky <igorkarbachinsky@mail.ru>
2014-05-05 00:35:24 +0400

    source=main for change and add articles and organizations

--- gorod/admin.py
+++ gorod/admin.py
@@ -1,29 +1,60 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 from gorod.models import City, Article, ArticleRubric, Organization, OrganizationCategory, CityInfo
 
-class CityAdmin(admin.ModelAdmin):
+
+class GorodAdminBase(admin.ModelAdmin):
+
+    def response_add(self, request, obj):
+        """ Redirect to object's page after add if exists param source  = main """
+        return self._response_add_and_change(request, obj)
+
+    def response_change(self, request, obj):
+        """ Redirect to object's page after save change if exists param source = main """
+        return self._response_add_and_change(request, obj)
+
+    def _response_add_and_change(self, request, obj):
+        """ if user clicked "edit this page" or 'add page ' return to obj's page  after save """
+        response = super(GorodAdminBase, self).response_change(request, obj)
+
+        if (isinstance(response, HttpResponseRedirect) and
+            request.GET.get('source') == 'main'):
+                response['location'] = reverse(
+                    'gorod:' + self._url_name_by_object(obj), 
+                    args=[obj.city.name, obj.id]
+                    #kwargs={'article_id': obj.id, 'city_name': obj.city.name}
+                )
+
+        return response
+
+    def _url_name_by_object(self, obj):
+        """ get url name for object by it's class name"""
+        return obj.__class__.__name__.lower()
+
+
+class CityAdmin(GorodAdminBase):
     list_display = ('id', 'name', 'title', 'add_date')
 
 
-class CityInfoAdmin(admin.ModelAdmin):
+class CityInfoAdmin(GorodAdminBase):
     list_display = ('id', 'city')
 
 
-class ArticleAdmin(admin.ModelAdmin):
+class ArticleAdmin(GorodAdminBase):
     list_display = ('id', 'add_date', 'title', 'city', 'rubric', 'user')
 
 
-class ArticleRubrucAdmin(admin.ModelAdmin):
+class ArticleRubrucAdmin(GorodAdminBase):
     list_display = ('id', 'name')
 
 
-class OrganizationAdmin(admin.ModelAdmin):
+class OrganizationAdmin(GorodAdminBase):
     list_display = ('id', 'name', 'city', 'user', 'add_date')
 
-class OrganizationCategoryAdmin(admin.ModelAdmin):
+class OrganizationCategoryAdmin(GorodAdminBase):
     list_display = ('id', 'name', 'title')
-
 
 
 

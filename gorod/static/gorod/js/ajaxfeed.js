061ad24907250bea18b049fb1a8d69ea86bf15a4
k.kalchenko <k.kalchenko@corp.mail.ru>
2014-07-16 14:03:44 +0400

    ajax feed

---
+++ gorod/static/gorod/js/ajaxfeed.js
@@ -0,0 +1,65 @@
+(window.ajaxFeed = function(){
+	var getData = function(page,limit){
+		return $.ajax({
+			url : '/api/feed/',
+			data : {
+				limit : limit,
+				page : page,
+				city : globals.cityName,
+				rubric : globals.rubric,
+				user : globals.profileUserId
+			}
+		});
+	},
+	appendElems = function(data){	
+		var appendedHtml= $(''),
+		curElem;
+
+		$.each(data.feed,function(index, elem){
+			curElem = $(template(elem));
+			appendedHtml = appendedHtml.add(curElem);
+		});
+		
+		$container.append(appendedHtml);
+		$container.masonry( 'appended', appendedHtml );
+		$container.imagesLoaded( function() {
+			$container.masonry();
+
+		});	
+		//$('.b-feed__more').show();
+	},
+	uploadNew = function(){
+		getData(curPage,LIMIT).done(appendElems).done(function(data){
+
+			$('.b-feed').height('auto');
+			pagesLen = data.total%LIMIT > 0 ? parseInt(data.total/LIMIT)+1 : parseInt(data.total/LIMIT);
+
+			if(curPage+1 < pagesLen){
+				$('.b-feed__more').show();
+			}else{
+				$('.b-feed__more').hide();
+			}
+			curPage++;
+		});
+	},
+	source   = $("#feed-item-template").html(),
+	template = Handlebars.compile(source),
+	curPage=0,
+	LIMIT = 10,
+	$container = $('.b-feed__list'),
+	pagesLen;
+
+	
+	uploadNew();
+	$('.b-feed__more').hide();
+	$('.b-feed__more').on('click',function(){
+		uploadNew();
+	});
+	$('.b-feed__list').masonry({
+		columnWidth: '.grid-sizer',
+		itemSelector: '.b-feed-item',
+		gutter : '.gutter-sizer',
+		transitionDuration: 0
+	});
+});
+

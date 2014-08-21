d0ed86a46179b9b3ba9f4496b2f7780b393d2364
k.kalchenko <k.kalchenko@corp.mail.ru>
2014-08-21 12:11:45 +0400

    form

--- gorod/static/gorod/js/addpopup.js
+++ gorod/static/gorod/js/addpopup.js
@@ -96,6 +96,9 @@
             getFormHtml(url, name, title, 'edit');
             formType = 'edit';
         });
+        $window.on('click', '.b-form__del',function(){
+            location.href=window.getEnv('articleDeleteUrl');
+        });
 
     },
 
@@ -111,6 +114,8 @@
                 $previewTools = $popup.find('.b-form__preview-tools');
                 $previewTools.addClass('b-form__preview-tools_active');
                 $('.b-form__preview').css("background-image", "url(/media/"+$('.b-form__prev-picture').text()+")");
+                $popup.find('.b-form__del').show();
+
             }
         });
         $popup.addClass(shownClass);

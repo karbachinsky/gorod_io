29070617b0bcc262ced9b1966d48e1c6c42a7cfa
k.kalchenko <k.kalchenko@corp.mail.ru>
2014-09-21 19:01:28 +0400

    source maps

--- etc/static_serv/Gruntfile.js
+++ etc/static_serv/Gruntfile.js
@@ -1,32 +1,33 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
  
  
-    compass: {
-		  dist: {
-  			options: {
-  				basePath:'../../gorod/static/gorod/',
-				sassDir:'scss',
-				cssDir:'css',
-				outputStyle:'compressed'
-  			}
-		  }
+		compass: {
+			dist: {
+				options: {
+					basePath:'../../gorod/static/gorod/',
+					sassDir:'scss',
+					cssDir:'css',
+					outputStyle:'compressed',
+					sourcemap: true
+				}
+			}
 	 },
  
-    watch: {
-      compass: {
-        files: '../../gorod/static/gorod/scss/*.scss', // следить за изменениями любых файлов с разширениями .scss
-        tasks: ['compass'] // и запускать такую задачу при их изменении
-      }
-    }
+		watch: {
+			compass: {
+				files: '../../gorod/static/gorod/scss/*.scss', // следить за изменениями любых файлов с разширениями .scss
+				tasks: ['compass'] // и запускать такую задачу при их изменении
+			}
+		}
  
-  });
+	});
  
-  //погружаем все необходимые модули
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-compass');
+	//погружаем все необходимые модули
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass');
  
-  //забиваем в задачу по умолчению все наши задачи
-  grunt.registerTask('default', ['compass', 'watch']);
+	//забиваем в задачу по умолчению все наши задачи
+	grunt.registerTask('default', ['compass', 'watch']);
 };

885b549177fe1ce900b55abdb26981fbcee68482
k.kalchenko <k.kalchenko@corp.mail.ru>
2014-10-07 22:59:27 +0400

    payment plate

--- gorod/utils/ad/plate.py
+++ gorod/utils/ad/plate.py
@@ -8,7 +8,7 @@ from gorod.models import Organization, HubQuestion, Payment
 
 class AdStartPlate(object):
     def __init__(self, city_name):
-        self.organizations = Organization.objects.filter(city__name=city_name).order_by('?')[0:8]
+        self.organizations = Organization.objects.filter(city__name=city_name).order_by('?')[0:4]
         self.questions = HubQuestion.objects.filter(city__name=city_name).order_by('?')[0:4]
         self.payments = Payment.objects.filter(city__name=city_name).order_by('?')[0:8]
 
